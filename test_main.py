efca8542f1c32dfb09a1c7abb42f2d1fef51c077
Jyrki Ahpola <jahpola@users.noreply.github.com>
2026-09-03 08:48:55 +0300

    Make Mr Ruff happy (#28)

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
-import unittest
-from unittest.mock import patch, MagicMock
 import datetime
+import unittest
+from unittest.mock import MagicMock, patch
 
 from botocore.exceptions import ClientError
 
